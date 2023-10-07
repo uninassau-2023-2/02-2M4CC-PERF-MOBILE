9a7e526576de31faa5d2e06fda10137d57b8abd6
iagolbdc <=>
2023-10-07 17:52:35 -0300

    Primeiro commit

---
+++ GRP25/SistemaDeAtendimento/src/app/tab2/tab2.page.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor(public senhasService: SenhasService) {}
+
+  chamarSenha(){
+    let senhaChamada: string = '';
+
+    if(this.senhasService.senhasArray.SP.length > 0){
+      senhaChamada = this.senhasService.senhasArray.SP.shift();
+    }else if(this.senhasService.senhasArray.SE.length > 0){
+      senhaChamada = this.senhasService.senhasArray.SE.shift();
+    }else if(this.senhasService.senhasArray.SG.length > 0){
+      senhaChamada = this.senhasService.senhasArray.SG.shift();
+    }
+
+    if(senhaChamada !== ''){
+      this.senhasService.senhasAtendidas.push(senhaChamada);
+    }
+
+    console.log(this.senhasService.senhasAtendidas)
+  }
+
+}
