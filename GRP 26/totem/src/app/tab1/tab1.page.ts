b5f71016d84587d5388f8fd528000cd1dcb0907e
Charles Junior <charles.sales@townsq.com.br>
2023-10-08 16:25:53 -0300

    projeto

---
+++ GRP 26/totem/src/app/tab1/tab1.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+
+export class Tab1Page {
+  
+  inputNovaSenha: string = '';
+
+  constructor(public senhasService: SenhasService) {}
+
+}
+
