bc9ec8c28c6e581c8ba3d8dd0d85101748ac3b3e
Skagth <105684891+Skagth@users.noreply.github.com>
2023-10-08 21:06:22 -0300

    projeto

--- GRP 12/totem/src/app/tab2/tab2.page.ts
+++ GRP 12/totem/src/app/tab2/tab2.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  private tipoSenhaAtual: string = 'SP';
+  public teste: number = 0;
 
-  constructor() {}
+  constructor(public senhasService: SenhasService) {}
 
+  chamarProxSenha() {
+    let senhaChamada: string = '';
+
+    if (this.senhasService.senhasArray[this.tipoSenhaAtual].length > 0) {
+      senhaChamada = this.senhasService.senhasArray[this.tipoSenhaAtual].shift();
+      this.senhasService.senhasChamadas.push(senhaChamada);
+    }
+
+
+    if (this.tipoSenhaAtual === 'SP') {
+      this.tipoSenhaAtual = 'SE';
+    } else if (this.tipoSenhaAtual === 'SE') {
+      this.tipoSenhaAtual = 'SG';
+    } else {
+      this.tipoSenhaAtual = 'SP';
+    }
+
+  }
 }
