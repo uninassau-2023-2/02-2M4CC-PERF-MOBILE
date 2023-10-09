98c3997387fba1de19238e4c1920952777654157
gabrielnleao <132684230+gabrielnleao@users.noreply.github.com>
2023-10-08 22:24:09 -0300

    Add files via upload

---
+++ GRP 7/src/app/tab1/tab1.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  
+  constructor(public senhasService: SenhasService) {}
+
+}
