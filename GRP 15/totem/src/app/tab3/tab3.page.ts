ee269a8bc509a283df3632f5a87e9d1306452e6e
João Ferreira <joaoferreirape@gmail.com>
2023-10-16 10:08:44 -0300

    Corrigindo Grupo 15

---
+++ GRP 15/totem/src/app/tab3/tab3.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+  constructor(public senhasService: SenhasService) {}
+
+}
