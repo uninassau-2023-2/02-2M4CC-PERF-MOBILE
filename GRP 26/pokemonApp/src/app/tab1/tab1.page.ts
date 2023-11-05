7fef3ef0d1acdffb0d7378c9fa4aca4ba00a75e2
Charles Junior <charles.sales@townsq.com.br>
2023-11-05 20:25:46 -0300

    pokemonApp task complted

---
+++ GRP 26/pokemonApp/src/app/tab1/tab1.page.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { PokeAPIService } from './../services/poke-api.service';
+import { ViaCEPService } from './../services/via-cep.service';
+import { forkJoin } from 'rxjs';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  areaBuscarPokemon: string = '52011210';
+  areaBusca: any = {
+    bairro: '',
+    localidade: '',
+    logradouro: '',
+    uf: ''
+  };
+  pokemonData: any = {
+    name: '',
+    image: '',
+    abilities: 0,
+    height: 0,
+    weight: 0
+  };
+
+  constructor(
+    private pokeAPIService: PokeAPIService,
+    private viaCEPService: ViaCEPService
+  ) {}
+
+  buscarInformacoes() {
+    const randomId = Math.floor(Math.random() * 100) + 1; 
+
+    forkJoin([
+      this.pokeAPIService.getPokeAPIService(randomId),
+      this.viaCEPService.getViaCEPService(this.areaBuscarPokemon)
+    ]).subscribe(([pokemonResponse, cepResponse]) => {
+      const pokemonData = pokemonResponse as any;
+      const cepData = cepResponse as any;
+
+      this.pokemonData.name = pokemonData.name.toUpperCase();
+      this.pokemonData.image = pokemonData.sprites.other['dream_world'].front_default;
+      this.pokemonData.abilities = pokemonData.abilities.length;
+      this.pokemonData.height = pokemonData.height;
+      this.pokemonData.weight = pokemonData.weight;
+
+      this.areaBusca.logradouro = cepData.logradouro;
+      this.areaBusca.bairro = cepData.bairro;
+      this.areaBusca.localidade = cepData.localidade;
+      this.areaBusca.uf = cepData.uf;
+    });
+  }
+}
