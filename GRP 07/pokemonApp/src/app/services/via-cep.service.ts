c68816a19056735a265939e58f7da033a0d2cc0f
gabrielnleao <132684230+gabrielnleao@users.noreply.github.com>
2023-11-26 19:16:55 -0300

    Correção dos arquivos para lista 7

--- GRP 07/pokemonApp/src/app/services/via-cep.service.ts
+++ GRP 07/pokemonApp/src/app/services/via-cep.service.ts
@@ -4,10 +4,9 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ViaCEPService {
-
-  constructor(public httpClient: HttpClient) { }
-  getViaCEPService(cep: string = '52011210') {
-    return this.httpClient.get(`https://viacep.com.br/ws/${cep}/json/`)
+export class ViaCepService {
+  constructor(private httpClient: HttpClient) { }
+  getViaCEPService (cep: string = '52011210') {
+    return this.httpClient.get(`https://viacep.com.br/ws/${cep}/json/`);
   }
 }
