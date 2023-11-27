f573e1079fe913d704e5bf716d496e6896c4701e
GuilhermeLeal <guilhermegordilho@gmail.com>
2023-11-27 19:58:21 -0300

    consertando lista7

--- GRP 04/pokemonApp/src/app/tab1/tab1.page.ts
+++ GRP 04/pokemonApp/src/app/tab1/tab1.page.ts
@@ -1,37 +1,76 @@
 import { Component } from '@angular/core';
 import { PokeAPIService } from '../services/poke-api.service';
-import { ViaCEPService } from '../services/via-cep.service';
+import { ViaCepService } from '../services/via-cep.service';
+import { TabsPage } from '../tabs/tabs.page';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+  styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  areaBuscarPokemon: string = '55011210';
+  areaBuscarPokemon: string = '52011210';
   areaBusca: any = {
     bairro: '',
-    locaidade: '',
+    localidade: '',
     logradouro: '',
-    uf: '',
+    uf: ''
   };
+  pokemonList: any[] = [];
+
+  idPokemon: number | undefined;
+   
+  pokemonAlternativo2: any = {
+    name: '',
+    sprites: '',
+    abilities: '',
+    height: '',
+    weight: '',
+  }
+
+  pokemon: any = {
+    name: '',
+    sprites: '',
+    abilities: '',
+    height: '',
+    weight: '',
+  }
+
+ 
+  pokemonAlternativo: any = {
+    sprites: '', 
+    abilities: '',
+    height: '',
+    weight: '',
+  }
   constructor(
-    private PokeAPIService: PokeAPIService,
-    private ViaCEPService: ViaCEPService
+    private pokeAPIService: PokeAPIService,
+    private viaCEPService: ViaCepService,
+    private tabsPage: TabsPage,
   ) {}
-  buscarPokemon() {
-    this.ViaCEPService.getViaCEPService(this.areaBuscarPokemon).subscribe(
-      (value) => {
-        this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))[
-          'logradouro'
-        ];
-        this.areaBusca.bairro = JSON.parse(JSON.stringify(value))['bairro'];
-        this.areaBusca.localidade = JSON.parse(JSON.stringify(value))[
-          'localidade'
-        ];
-        this.areaBusca.uf = JSON.parse(JSON.stringify(value))['uf'];
-      }
-    );
-    this.PokeAPIService.getPokeAPIService();
+buscarPokemon() {
+  this.viaCEPService.getViaCEPService(this.areaBuscarPokemon)
+  .subscribe((value) => {
+    this.areaBusca.logradouro = JSON.parse(JSON.stringify(value)) ['logradouro'];
+    this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value)) ['bairro'];
+    this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value)) ['localidade'];
+    this.areaBusca.uf = '-' + JSON.parse(JSON.stringify(value)) ['uf'];
+  });
+  this.pokeAPIService.getPokeAPIService(this.idPokemon)
+  .subscribe((value) => {
+    this.pokemon.name = JSON.parse(JSON.stringify(value)) ['name'];
+    this.pokemon.sprites = JSON.parse(JSON.stringify(value)) ['sprites'];
+
+    const abilitiesArray = JSON.parse(JSON.stringify(value))['abilities'];
+    
+    this.pokemonAlternativo.abilities = abilitiesArray.length;
+    this.pokemonAlternativo.height = JSON.parse(JSON.stringify(value)) ['height'];
+    this.pokemonAlternativo.weight = JSON.parse(JSON.stringify(value)) ['weight'];
+    this.tabsPage.updateHabilidadeTab1(this.pokemonAlternativo.abilities);
+    
+    
+
+    this.pokeAPIService.addPokemonData(this.pokemon);
+  })
   }
 }
